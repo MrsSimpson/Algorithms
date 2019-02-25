ff08485bbadc776910e36b5f4beb91ec9437e9c0
Lacy <lacy.simpson@usm.edu>
2019-02-25 10:00:18 -0600

    Implemented my own version of the insertion sort that is different from the book.

--- SortingProject1/source/main.py
+++ SortingProject1/source/main.py
@@ -9,6 +9,11 @@ def main():
     print(test_array2)
     insertion_sort(test_array3)
     print(test_array3)
+
+    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
+    another_insertions_sort(test_array3)
+    print(test_array3)
+
     test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
     bubble_sort(test_array3)
     print(test_array3)
@@ -27,7 +32,7 @@ def selection_sort(test_array):
 
     return test_array
 
-#Finished
+#Books implementation of insertion sort
 def insertion_sort(test_array):
     array_length = len(test_array)
     if array_length != 0 or array_length != 1:
@@ -42,6 +47,21 @@ def insertion_sort(test_array):
             test_array[j + 1] = current
 
         return test_array
+
+
+""" My implementation of an insertion sorting algorithm"""
+def another_insertions_sort(test_array):
+    array_len = len(test_array)
+    if test_array != None and test_array != 1:
+        for i in range(1, array_len):
+            index2 = test_array[i]
+            inner_range = i
+            for j in range ((i-1), -1, -1):
+                index1 = test_array[j]
+                index2 = test_array[inner_range]
+                if index1 > index2:
+                    test_array[j], test_array[inner_range] = test_array[inner_range], test_array[j]
+                inner_range = inner_range - 1
 
 def bubble_sort(test_array):
     for i in range(0, len(test_array) - 1):
