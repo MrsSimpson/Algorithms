6e90a5f7be521b59c885a46dc1f0ea83ee5737f5
Lacy Simpson <lacy.simpson@usm.edu>
2019-02-27 10:46:58 -0600

    added an array initializing function to main for testing purposes.

--- SortingProject1/source/main.py
+++ SortingProject1/source/main.py
@@ -6,35 +6,43 @@ from source import heap_sort
 from source import quick_sort
 
 
+def init_arrays(array1, array2, array3, array4):
+    array1 = []
+    array2 = [20]
+    array3 = [60, 30]
+    array4 = [20, 30, 10, 80, 60, 50, 40, 70]
+
+    return array1, array2, array3, array4
+
 def main():
+    array1 = []
+    array2 = [20]
+    array3 = [60, 30]
+    array4 = [20, 30, 10, 80, 60, 50, 40, 70]
 
-    test_array1 = []
-    test_array2 = [60, 30]
-    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
 
+    selection_sort(array4)
+    print(array4)
 
-    selection_sort(test_array3)
-    print(test_array3)
+    array1, array2, array3, array4 = init_arrays(array1, array2, array3, array4)
+    my_selection_sort(array3)
+    print(array3)
 
-    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
-    my_selection_sort(test_array3)
-    print(test_array3)
+    array1, array2, array3, array4 = init_arrays(array1, array2, array3, array4)
+    insertion_sort(array3)
+    print(array3)
 
-    insertion_sort(test_array3)
-    print(test_array3)
+    array1, array2, array3, array4 = init_arrays(array1, array2, array3, array4)
+    another_insertions_sort(array4)
+    print(array4)
 
-    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
-    another_insertions_sort(test_array3)
-    print(test_array3)
+    array1, array2, array3, array4 = init_arrays(array1, array2, array3, array4)
+    bubble_sort(array4)
+    print(array4)
 
-    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
-    bubble_sort(test_array3)
-    print(test_array3)
-
-    test_array3 = [20, 30, 10, 80, 60, 50, 40, 70]
-
-    initial_merge_sort(test_array3)
-    print(test_array3)
+    array1, array2, array3, array4 = init_arrays(array1, array2, array3, array4)
+    initial_merge_sort(array2)
+    print(array2)
 
 
 main()
